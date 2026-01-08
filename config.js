fc1b9178a6e5d4e72ae96a6572d200259f99c8ca
btcyberpubli <btcyberpubli@gmail.com>
2026-01-08 13:22:34 -0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,8 @@
+const BUSINESS_CONFIG = {
+  businessName: 'Brightfield Commerce Ltd',
+  businessAddress: '128 Trent Bridge Road, Nottingham, NG2 3BU, United Kingdom',
+  businessPhone: '+44 115 947 5000',
+  businessEmail: 'support@brightfieldcommerce.co.uk',
+  websiteDomain: 'brightfieldcommerce.co.uk',
+  businessType: 'E-commerce Services'
+};
